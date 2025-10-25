4361166f6e81830914d7612bd5038ac70773c425
Jackson Green <freestyler895@gmail.com>
2025-10-25 14:51:01 -0500

    remove fontawesome packages

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ This is a fun trivia application built using the API for the [Open Trivia Databa
 
 Just a few things I hope to add as I get the time.
 
+- [x] Remove fontawesome
 - [ ] Enter/leave animations on question cards
 - [ ] Dark mode
 - [ ] Different themes
+- [ ] Test different API's
 - [ ] More filters
 - [ ] Resource API integration when it's finalized
 - [ ] Signal forms when they arrive?
--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,19 +1,13 @@
 import { ChangeDetectionStrategy, Component, ElementRef, viewChild } from '@angular/core';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faQuestion , faClose } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
-  imports: [FaIconComponent],
   templateUrl: './about.html',
   styleUrl: './about.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class About {
   public readonly dialog = viewChild.required<ElementRef<HTMLDialogElement>>('aboutDialog');
-
-  protected infoIcon = faQuestion
-  protected closeIcon = faClose
 
   protected closeDialog(): void {
     this.dialog().nativeElement.close();
